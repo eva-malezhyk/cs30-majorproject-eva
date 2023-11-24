54b56957c782f9d86293cdb313f597dcc0a9d71c
eva-malezhyk <evamal1201@gmail.com>
2023-11-24 11:44:11 -0600

    working on major project

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
-// Project Title
+// EJ's sushiria
 // Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+let backg;
+
+function preload(){
+  backg = loadImage("")
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
